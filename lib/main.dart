e1535c5a7e96fb14179c63b5f30e7321bd33ff1a
zakiburnama <zaki.burnama1017@gmail.com>
2021-12-05 08:07:26 +0700

    progress listview json but still error :(

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+//import 'package:provider/provider.dart';
 
-import 'src/catalog.dart';
-import 'src/item_tile.dart';
+//import 'src/catalog.dart';
+//import 'src/item_tile.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+//import './model.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter MPR Delta',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future fetchNotes() async{
+    var url = 'https://raw.githubusercontent.com/boriszv/json/master/random_example.json';
+    var response = await http.get(url);
+    if 
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter MPR delta'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index){
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                children: <Widget>[
+                  Text('data'),
+                  Text('data'),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: 50,
+      ),
+    );
+  }
+
+}
+
+
+//
+/*
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -21,119 +86,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-/*
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-*/
-/*
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  /*
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-  */
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Selector<Catalog, int?>(
-        selector: (context, catalog) => catalog.itemCount,
-        builder: (context, itemCount, child) => ListView.builder(
-          itemCount: itemCount,
-          padding: const EdgeInsets.symmetric(vertical: 18),
-          itemBuilder: (context, index) {
-            var catalog = Provider.of<Catalog>(context);
-            var item = catalog.getByIndex(index);
-            if (item.isLoading) {
-              return const LoadingItemTile();
-            }
-            return ItemTile(item: item);
-          },
-        ),
-        /*
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-        */
-      ),
-      /*
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-      */ // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -142,25 +94,174 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MPR delta'),
+        title: Text('MPR delta'),
       ),
-      body: Selector<Catalog, int?>(
-        selector: (context, catalog) => catalog.itemCount,
-        builder: (context, itemCount, child) => ListView.builder(
-          itemCount: itemCount,
-          padding: const EdgeInsets.symmetric(vertical: 18),
+      body: ListView.builder(
+        itemBuilder: (context, index){
 
-          itemBuilder: (context, index) {
-            var catalog = Provider.of<Catalog>(context);
-            var item = catalog.getByIndex(index);
-            
-            if (item.isLoading) 
-              return const LoadingItemTile();
-            
-            return ItemTile(item: item);
+        },
+      ),
+    );
+  }
+}
+*/
+
+//
+/*
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String data;
+  var superheros_length;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+  void getData() async {
+    http.Response response =
+        await http.get("https://protocoderspoint.com/jsondata/superheros.json");
+    if (response.statusCode == 200) {
+      data = response.body; //store response as string
+      setState(() {
+        superheros_length = jsonDecode(
+            data)['superheros']; //get all the data from json string superheros
+        print(superheros_length.length); // just printed length of data
+      });
+      var venam = jsonDecode(data)['superheros'][4]['url'];
+      print(venam);
+    } else {
+      print(response.statusCode);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Http Example"),
+      ),
+      body: ListView.builder(
+        itemCount: superheros_length == null ? 0 : superheros_length.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: ListTile(
+              leading: Image.network(
+                jsonDecode(data)['superheros'][index]['url'],
+                fit: BoxFit.fill,
+                width: 100,
+                height: 500,
+                alignment: Alignment.center,
+              ),
+              title: Text(jsonDecode(data)['superheros'][index]['name']),
+              subtitle: Text(jsonDecode(data)['superheros'][index]['power']),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+*/
+
+
+/*
+class Produk extends StatefulWidget {
+  @override
+  _ProdukState createState() => new _ProdukState();
+}
+
+class _ProdukState extends State<Produk> {
+  final String sUrl = "https://mysimrs.com/api/";
+  List<ProdukModel> listproduk;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<List<ProdukModel>> _fetchData() async {
+    var params = "produk.php";
+    try {
+      var jsonResponse = await http.get(sUrl + params);
+      if (jsonResponse.statusCode == 200) {
+        final jsonItems =
+            json.decode(jsonResponse.body).cast<Map<String, dynamic>>();
+
+        listproduk = jsonItems.map<ProdukModel>((json) {
+          return ProdukModel.fromJson(json);
+        }).toList();
+      }
+    } catch (e) {}
+    return listproduk;
+  }
+
+  Future<Null> _refresh() {
+    return _fetchData().then((_listproduk) {
+      setState(() => listproduk = _listproduk);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.orange,
+        title: Text(' Data Produk'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: FutureBuilder<List<ProdukModel>>(
+          future: _fetchData(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Center(child: CircularProgressIndicator());
+            return Container(
+              margin: EdgeInsets.only(bottom: 0.0),
+              child: ListView(
+                padding: EdgeInsets.only(bottom: 160.0),
+                children: snapshot.data
+                    .map(
+                      (_data) => Column(children: <Widget>[
+                        Card(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                leading: Icon(Icons.perm_media, size: 50),
+                                title: Text(_data.nama),
+                                subtitle:
+                                    Text(_data.harga + ' / ' + _data.satuan),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]),
+                    )
+                    .toList(),
+              ),
+            );
           },
         ),
       ),
     );
   }
 }
+*/
